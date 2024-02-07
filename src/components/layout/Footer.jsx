6b044962ced0359847cbb7bae753ad743bac63b1
unknown <0803jiyoung99@gmail.com>
2024-02-07 20:40:21 +0900

    git config core.ignorecase false 설정

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,8 @@
+import { StFooter } from "Style";
+import React from "react";
+
+function Footer() {
+  return <StFooter>© 2024. Zyoung Co. all rights reserved.</StFooter>;
+}
+
+export default Footer;
