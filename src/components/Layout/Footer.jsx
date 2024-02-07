2cf52f7d317d6d188b3960bb637a6de74c385057
JIYOUNG <154482077+Zzzzyoung@users.noreply.github.com>
2024-02-07 20:43:54 +0900

    Delete src/components/Layout directory

--- src/components/Layout/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import { StFooter } from "Style";
-import React from "react";
-
-function Footer() {
-  return <StFooter>© 2024. Zyoung Co. all rights reserved.</StFooter>;
-}
-
-export default Footer;
